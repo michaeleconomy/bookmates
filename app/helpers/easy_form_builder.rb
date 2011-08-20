284e083b85e5b605803edf6bfe427cebd0ae2b14
michael economy <michael@michaeleconomy.com>
2011-08-19 17:00:55 -0700

    fix for ruby 1.9

--- app/helpers/easy_form_builder.rb
+++ app/helpers/easy_form_builder.rb
@@ -2,7 +2,7 @@ class EasyFormBuilder < ActionView::Helpers::FormBuilder
   include ActionView::Helpers::TagHelper
   class_eval do
     %w(text_field password_field text_area check_box select calendar_date_select).each do |name|
-      define_method name do |*args|
+      define_method name do |args|
         attribute, options = args
         if options.is_a?(Hash)
           extra_stuff = options.delete :extra_stuff
